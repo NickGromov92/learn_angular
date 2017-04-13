e96d6e0c48cb7b11d1573e0ffe985105492de791
Nick Gromov <Nick.Gromov.92@gmail.com>
2017-04-13 17:51:06 +0300

    First commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+    title = 'FIRST APP!';
+    todos = todos;
 }
+
+const todos = [
+    "Изучить Java Script",
+    "Изучить Angular",
+    "Написать приложение"
+];
