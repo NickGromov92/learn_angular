a44f2a8573c6364c6ce9b0d7d3f175cc3ff8b481
Nick Gromov <nick.gromov.92@gmail.com>
2017-04-18 02:11:20 +0300

    after easter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,58 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Event} from "_debugger";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-    title = 'FIRST APP!';
-    todos = todos;
+  title = 'FIRST APP!';
+  todos: Todo[] = todos;
+
+  newTodoTitle: string;
+
+  toggle(todo: Todo) {
+    todo.status = !todo.status;
+  };
+
+  delete(todo: Todo) {
+    let index = this.todos.indexOf(todo);
+
+    if (index > -1) {
+      this.todos.splice(index, 1);
+    }
+  };
+
+  create() {
+
+
+    let todo: Todo = new Todo(this.newTodoTitle);
+
+    this.todos.push(todo);
+
+    this.newTodoTitle = '';
+  }
+
 }
 
-const todos = [
-    "Изучить Java Script",
-    "Изучить Angular",
-    "Написать приложение"
+class Todo {
+  constructor (public title: string,
+               public status: boolean = false){}
+}
+
+const todos: Todo[] = [
+  {
+    title: "Изучить Java Script",
+    status: true
+  },
+  {
+    title: "Изучить Angular",
+    status: false
+  },
+  {
+    title: "Написать приложение",
+    status: false
+  }
 ];
